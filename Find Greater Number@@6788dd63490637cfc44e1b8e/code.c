1f0f7ed41d9ba14fde5f31967becd43373a01a18
garvita sudan <garvita1147.becse24@chitkara.edu.in>
2025-02-09 14:12:05 +0530

    Solution to Find Greater Number

--- Find Greater Number@@6788dd63490637cfc44e1b8e/code.c
+++ Find Greater Number@@6788dd63490637cfc44e1b8e/code.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main() {
     int num1,num2,num3,max;
-    printf("enter three numbers:")
+    printf("enter three numbers:");
     scanf("%d,%d,%d",&num1,&num2, &num3);
     if(num1>=num2 && num1 >=num3){
        max = num1;
