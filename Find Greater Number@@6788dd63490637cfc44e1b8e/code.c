ed2df8503ab43d7f96fdd386db5ee9d8c5e71332
garvita sudan <garvita1147.becse24@chitkara.edu.in>
2025-02-09 14:13:04 +0530

    Solution to Find Greater Number

--- Find Greater Number@@6788dd63490637cfc44e1b8e/code.c
+++ Find Greater Number@@6788dd63490637cfc44e1b8e/code.c
@@ -6,9 +6,9 @@ int main() {
     if(num1>=num2 && num1 >=num3){
        max = num1;
     } else if(num2 >= num1 && num2 >=num3){
-        max = num2;
+       max = num2;
     } else {
-        max = num3;
+       max = num3;
     }
     printf("the greatest number is: %d\n",max);
 
